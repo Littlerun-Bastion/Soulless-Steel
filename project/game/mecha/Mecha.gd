bb660f50f722decfc9572cd2ebc477bbe2a23c04
Ricardo Lira <pudimdefrutas@gmail.com>
2021-06-03 15:27:28 -0300

    Added rotation to mechas (instant for now)

--- project/game/mecha/Mecha.gd
+++ project/game/mecha/Mecha.gd
@@ -3,5 +3,6 @@ class_name Mecha
 
 var max_speed = 200
 var friction = 0.1
-var acceleration = 0.1
+var move_acc = 0.1
+var rotation_acc = 0.1
 var velocity = Vector2()
--- project/game/mecha/Player.gd
+++ project/game/mecha/Player.gd
@@ -6,14 +6,21 @@ func _ready():
 	setup()
 
 func _physics_process(_delta):
+	#Movement
 	var direction = get_input()
 	
 	if direction.length() > 0:
-		velocity = lerp(velocity, direction.normalized() * max_speed, acceleration)
+		velocity = lerp(velocity, direction.normalized() * max_speed, move_acc)
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, friction)
 	
 	velocity = move_and_slide(velocity)
+	
+	#Rotation
+	var mouse_pos = get_global_mouse_position()
+	var target_rot = rad2deg(global_position.angle_to_point(mouse_pos)) - 90
+	rotation_degrees = target_rot
+	#rotation_degrees = lerp(rotation_degrees, target_rot, rotation_acc)
 
 
 func _input(event):

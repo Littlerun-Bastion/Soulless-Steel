e7f13994452f4b615ac67d8320faa13cb14ab857
Ricardo Lira <pudimdefrutas@gmail.com>
2021-06-09 17:36:48 -0300

    Properly rotating clock or counter-clockwise to mouse position

--- project/game/mecha/Mecha.gd
+++ project/game/mecha/Mecha.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 class_name Mecha
 
+var movement_type = "free"
 var max_speed = 200
 var friction = 0.1
-var move_acc = 0.1
-var rotation_acc = 0.1
+var move_acc = 50
+var rotation_acc = 800
 var velocity = Vector2()
--- project/game/mecha/Player.gd
+++ project/game/mecha/Player.gd
@@ -5,27 +5,43 @@ onready var Cam = $Camera2D
 func _ready():
 	setup()
 
-func _physics_process(_delta):
-	#Movement
-	var direction = get_input()
-	
-	if direction.length() > 0:
-		velocity = lerp(velocity, direction.normalized() * max_speed, move_acc)
-	else:
-		velocity = lerp(velocity, Vector2.ZERO, friction)
-	
-	velocity = move_and_slide(velocity)
-	
-	#Rotation
-	var mouse_pos = get_global_mouse_position()
-	var target_rot = rad2deg(global_position.angle_to_point(mouse_pos)) - 90
-	rotation_degrees = target_rot
-	#rotation_degrees = lerp(rotation_degrees, target_rot, rotation_acc)
+func _physics_process(delta):
+	apply_movement(delta)
+	apply_rotation(delta)
 
 
 func _input(event):
 	if event.is_action_pressed("honk"):
 		AudioManager.play_sfx("test", global_position)
+
+
+func apply_movement(dt):
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = lerp(velocity, direction.normalized() * max_speed, move_acc*dt)
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, friction)
+	
+	velocity = move_and_slide(velocity)
+
+
+func apply_rotation(dt):
+	var rot_eps = 5
+	var mouse_pos = get_global_mouse_position()
+	var target_rot = fmod(rad2deg(global_position.angle_to_point(mouse_pos)) + 270, 360)
+	var diff = target_rot - rotation_degrees
+	if diff > 180:
+		diff -= 360
+	elif diff < -180:
+		diff += 360
+	
+	#Rotate properly clock or counter-clockwise fastest to target rotation
+	if abs(diff) <= rot_eps:
+		rotation_degrees = target_rot
+	elif diff > 0:
+		rotation_degrees += rotation_acc*dt
+	else:
+		rotation_degrees -= rotation_acc*dt
 
 
 func setup():

dc90b2460566748006a737dcadec944d5ca6c5fc
Ricardo Lira <pudimdefrutas@gmail.com>
2021-07-08 15:53:05 -0300

    Added some accuracy to weapons

--- project/game/weapons/WeaponNode.gd
+++ project/game/weapons/WeaponNode.gd
@@ -20,8 +20,13 @@ func set_shooting_pos(pos):
 	ShootingPos.position = pos
 
 
-func get_direction():
-	return (ShootingPos.global_position - global_position).normalized()
+func get_direction(accuracy_margin := 0.0):
+	var offset = Vector2()
+	var dir = (ShootingPos.global_position - global_position).normalized()
+	if accuracy_margin > 0:
+		offset = dir.rotated(PI/2)*rand_range(-accuracy_margin, accuracy_margin)
+		dir = (ShootingPos.global_position + offset - global_position).normalized()
+	return dir
 
 
 func get_shoot_position():

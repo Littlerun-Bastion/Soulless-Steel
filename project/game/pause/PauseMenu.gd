8d6a9d15dc5877e0e2981d316a9b4fd68612cf63
Ricardo Lira <pudimdefrutas@gmail.com>
2023-01-10 15:24:25 -0300

    Removed pause screen buttons working during unpaused game
    Close #38

--- project/game/pause/PauseMenu.gd
+++ project/game/pause/PauseMenu.gd
@@ -3,13 +3,26 @@ extends CanvasLayer
 signal pause_toggle
 
 onready var ViewContainer = $ViewportContainer/Viewport/Control
+onready var ResumeButton = $ViewportContainer/Viewport/Control/MarginContainer/VBoxContainer/Resume
+onready var QuitButton = $ViewportContainer/Viewport/Control/MarginContainer/VBoxContainer/Quit
 
 func _ready():
 	$ViewportContainer.hide()
 	ViewContainer.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	disable()
 
 func is_paused():
 	return $ViewportContainer.visible
+
+
+func enable():
+	ResumeButton.disabled = false
+	QuitButton.disabled = false
+
+
+func disable():
+	ResumeButton.disabled = true
+	QuitButton.disabled = true
 
 
 func toggle_pause():
@@ -17,17 +30,20 @@ func toggle_pause():
 	$ParallaxBackground/GridLayer.visible = not $ParallaxBackground/GridLayer.visible
 	$ParallaxBackground/GridLayer2.visible = not $ParallaxBackground/GridLayer2.visible
 	if $ViewportContainer.visible:
+		enable()
 		ViewContainer.mouse_filter = Control.MOUSE_FILTER_STOP
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		ShaderEffects.play_transition(0, 1000, 2.0)
 	else:
+		disable()
 		ViewContainer.mouse_filter = Control.MOUSE_FILTER_IGNORE
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	
 	emit_signal("pause_toggle", $ViewportContainer.visible)
 
 func _on_Button_mouse_entered():
-	AudioManager.play_sfx("select")
+	if is_paused():
+		AudioManager.play_sfx("select")
 
 
 func _on_Quit_pressed():

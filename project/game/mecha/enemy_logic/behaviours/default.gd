78e9c882203964aee692cf1ae52d0b263beda39f
yosqiw <81264436+yosqiw@users.noreply.github.com>
2023-07-05 06:31:14 +0100

    Smart AI Part 1: Audio investigation
    The AI will roam until it hears a gunshot, where it will then move towards that gunshot. Right now though, it's pretty dumb.

--- project/game/mecha/enemy_logic/behaviours/default.gd
+++ project/game/mecha/enemy_logic/behaviours/default.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const POSITIONAL_ACCURACY = 200.0
+
 #Essential variables
 var nodes = ["roaming", "targeting"]
 var initial_state = "roaming"
@@ -7,6 +9,7 @@ var initial_state = "roaming"
 #Custom variables
 var engage_distance = 2000
 var max_shooting_distance = 3500
+var point_of_interest
 
 func get_nodes():
 	return nodes
@@ -27,15 +30,30 @@ func targeting_to_roaming(enemy):
 ## STATE METHODS ##
 
 func do_roaming(dt, enemy):
-	if enemy.senses.sounds.is_empty():
-		if not enemy.going_to_position:
+	if not enemy.going_to_position:
+		point_of_interest = enemy.arena.get_random_position()
+		enemy.going_to_position = true
+		
+	if enemy.get_most_recent_loud_noise():
+		if enemy.global_position.distance_to(enemy.get_most_recent_loud_noise().position) > POSITIONAL_ACCURACY:
+			point_of_interest = enemy.get_most_recent_loud_noise().position
 			enemy.going_to_position = true
-			enemy.NavAgent.target_position = enemy.arena.get_random_position()
+			#enemy.going_to_position = false
+			#point_of_interest = false
+			#else:
+	
+	if point_of_interest:		
+		if enemy.global_position.distance_to(point_of_interest) > POSITIONAL_ACCURACY:
+			enemy.NavAgent.target_position = point_of_interest
 	else:
-		if enemy.get_most_recent_loud_noise():
-			enemy.going_to_position = true
-			enemy.NavAgent.target_position = enemy.get_most_recent_loud_noise().position
+		print("No POI")
+	
 	enemy.navigate_to_target(dt)
+	
+	if enemy.NavAgent.is_navigation_finished():
+		enemy.going_to_position = false
+		point_of_interest = false
+		print("Point reached")
 	
 	
 	#enemy.check_for_targets(engage_distance, max_shooting_distance)

fc98f67a585d46610af9b48f909917e28e236b51
Ricardo Lira <pudimdefrutas@gmail.com>
2023-07-28 21:35:19 -0300

    Fixing main menu

--- project/game/start_menu/Intro.gd
+++ project/game/start_menu/Intro.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _ready():
-		TransitionManager.transition_to("res://game/start_menu/StartMenu.tscn", "Booting System.........")
+	TransitionManager.transition_to("res://game/start_menu/StartMenu.tscn", "Booting System.........")
